a32195b0a0599a1600df307386eb8ef0035df85e
Bartek <barsys@onet.pl>
2020-05-20 21:27:34 +0200

    first code

---
+++ unit303/src/main/java/com/sda/werehouse/unit303/Unit303Application.java
@@ -0,0 +1,14 @@
+package com.sda.werehouse.unit303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit303Application.class, args);
+		System.out.println("Start");
+	}
+
+}
