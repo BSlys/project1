1a872c7a0847ec33ab0fba72ee88f70e7f5e7162
Bartek <barsys@onet.pl>
2020-05-23 12:34:20 +0200

    first code - auth

---
+++ unit303/src/main/java/com/sda/werehouse/unit303/model/ItemRole.java
@@ -0,0 +1,14 @@
+package com.sda.werehouse.unit303.model;
+
+public enum ItemRole {
+
+    SECURE_BOOTS,
+    SECURE_JACKET,
+    SECURE_BACKPACK,
+    WEAPON_MAIN,
+    WEAPON_SIDE,
+    AMMO_MAIN,
+    AMMO_SIDE,
+    OTHER_FOOD,
+    OTHER_OTHER;
+}
