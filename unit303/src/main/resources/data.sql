5089f364a72c8b986a68f10b42c8b28f9a08242e
BSlys <barsys@onet.pl>
2020-06-06 12:24:51 +0200

    entry data

--- unit303/src/main/resources/data.sql
+++ unit303/src/main/resources/data.sql
@@ -9,10 +9,10 @@ insert into order_ent (id, accepted, item_id, quantity, user_id) values
 (2, false, 2, 1, 1),
 (3, false, 3, 2, 0);
 
-insert into order_archive_entry (id, item_id, quantity, user_id) values
-(1, 1, 4, 1),
-(2, 2, 1, 1),
-(3, 3, 2, 0);
+insert into order_archive_entry (id, item_id, quantity, user_id, post_time) values
+(1, 1, 4, 1, '2020-01-01 00:59:59.179214'),
+(2, 2, 1, 1, '2020-01-01 00:59:59.179214'),
+(3, 3, 2, 0, '2020-01-01 00:59:59.179214');
 
 insert into item (id, name, itemrole, quantity) values
 (1, 'Oficerki', 'SECURE_BOOTS', 10),
@@ -20,5 +20,5 @@ insert into item (id, name, itemrole, quantity) values
 (3, 'Plecak', 'SECURE_BACKPACK', 10),
 (4, 'granaty', 'WEAPON_SIDE', 50);
 
-insert into message_dto (id, sender, reciver, message, read) values
-(1, 'system', 0, 'Inicjalizacja konta administracyjnego', 'false');
+insert into message_dto (id, sender, reciver, message, read, date) values
+(1, 'system', 0, 'Inicjalizacja konta administracyjnego', 'false', '2020-01-01 00:59:59.179214');
